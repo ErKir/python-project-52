d1d12bbf98e7101e5c75725435e4f2625e1499ce
ErKir <kirillerohin@gmail.com>
2023-12-07 21:10:47 +0300

    ref: add render app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['webserver', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'render.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
